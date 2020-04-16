7b2893dcfed4805120008a79de5ebad81d5f2667
apolopena <penavilla@gmail.com>
2020-04-16 13:32:35 -0700

    Cmder Config file
    Copy and paste what you like into your /Cmder/config/user_aliases.cmd or /Cmder/config/user_profile.cmd, or replace the entire file(s).

---
+++ user_aliases.cmd
@@ -0,0 +1,19 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+dirs=find . -maxdepth 1 -type d | sed -e 's/^\.\///'
+reload=C:\cmder\vendor\init.bat
+winpaths=path | tr ; '\n'
+dd=if "$1" equ "" (printf "----\ndd\nusage:\nPass a single argument representing the root directory to search and all directories \nin that tree will be displayed, excluding the following:\nnode_modules\npublic\n.git\n.cache\n----\n") else find "$1" -type d \( -name "node_modules" -o -name "site-packages" -o -name "public" -o -name ".git" -o -name ".cache" \) -prune -o -print | sed -e '1d; s/^\.\//\//' | grep "$2" | sort -u
